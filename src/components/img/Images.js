b87b78363bd9fa38543a5917aeea5993e9f59d82
Adrian Castro <70278376+kastro0005@users.noreply.github.com>
2024-01-04 21:10:48 -0500

    qwer

--- src/components/img/Images.js
+++ src/components/img/Images.js
@@ -1,34 +1,17 @@
 //Imagenes de la Web por componente URLs:
 
-///////////////////////////////////////////////////////////////////////////
-//Borrar en producion:
-
-import logo_JY from "assets/img/Logo/logo.png";
-import img_notFound from "assets/img/Alert/NotFound.jpg";
-import DashBoardImg from "assets/img/Dashboard/Dash.jpeg";
-import LoginHome from "assets/img/Login/Home_img.jpg";
-
 //Alert: NotFount.jpg
-export const NotFound = img_notFound;
+export const NotFound =
+  "https://imagedelivery.net/a5TfjzZDPNM57yn2-rwdsg/53b09658-d450-4a24-0e58-be8c2e32e200/public";
 
 //Dashboard: Dash.jpg
-export const ImgDash = DashBoardImg;
+export const ImgDash =
+  "https://imagedelivery.net/a5TfjzZDPNM57yn2-rwdsg/85834a14-4dee-4027-87c6-bc7fc8f5a400/public";
 
 //Login: Home_img.jpg
-export const HomeImg = LoginHome;
+export const HomeImg =
+  "https://imagedelivery.net/a5TfjzZDPNM57yn2-rwdsg/c63ddd4e-be57-43d7-0371-5c2d11979d00/public";
 
 //Logo: logo.png
-export const Logo = logo_JY;
-
-////////////////////////////////////////////////////////////////////////////////////
-// //Alert: NotFount.jpg
-// export const NotFound = "";
-
-// //Dashboard: Dash.jpg
-// export const ImgDash = "";
-
-// //Login: Home_img.jpg
-// export const HomeImg = "";
-
-// //Logo: logo.png
-// export const Logo = "";
+export const Logo =
+  "https://imagedelivery.net/a5TfjzZDPNM57yn2-rwdsg/929d78f1-691d-428b-632f-d8368c743500/public";
